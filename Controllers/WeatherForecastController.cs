1a260ca5fc727e6b5a8555bbafc2cf7772b1ddee
Cihan <cnyildiri@coastal.edu>
2024-04-17 10:32:43 -0400

    done

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JSPromises.Controllers
+{
+    [ApiController]
+    [Route("decorations")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("border/{choice}")]
+        public string GetBorderColor(string choice)
+        {
+            Task.Delay(3000).Wait();
+            if (choice.Equals("a")){
+                return "thick dashed red";
+            }else{
+                return "thick dotted orange";
+            }
+        }
+
+        [HttpGet("bgcolor/{choice}")]
+        public string GetBGColor(string choice)
+        {
+            Task.Delay(3000).Wait();
+            if (choice.Equals("a")){
+                return "blue";
+            }else{
+                return "green";
+            }
+        }
+    }
+}
